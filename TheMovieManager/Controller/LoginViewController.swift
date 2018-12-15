f3cf325209d077e7e153e9f16129b4fa7880e0f4
Poman Kao <ayujolin520@gmail.com>
2018-12-15 15:31:00 +0800

    init

---
+++ TheMovieManager/Controller/LoginViewController.swift
@@ -0,0 +1,86 @@
+//
+//  LoginViewController.swift
+//  TheMovieManager
+//
+//  Created by Owen LaRosa on 8/13/18.
+//  Copyright © 2018 Udacity. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var loginViaWebsiteButton: UIButton!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        emailTextField.text = ""
+        passwordTextField.text = ""
+    }
+    
+    @IBAction func loginTapped(_ sender: UIButton) {
+        setLogginIn(true)
+        TMDBClient.getRequestToken(completion: handleRequestTokenResponse(success:error:))
+    }
+    
+    @IBAction func loginViaWebsiteTapped() {
+        setLogginIn(true)
+        TMDBClient.getRequestToken {
+            (success, error) in
+            if success {
+                UIApplication.shared.open(TMDBClient.Endpoints.webAuth.url, options: [:], completionHandler: nil)
+            }
+        }
+    }
+    
+    func handleRequestTokenResponse(success: Bool, error: Error?) {
+        if success {
+            print(TMDBClient.Auth.requestToken)
+            TMDBClient.login(username: self.emailTextField.text ?? "", password: self.passwordTextField.text ?? "", completion: self.handleLoginResponse(success:error:))
+        } else {
+            showLoginFailure(message: error?.localizedDescription ?? "")
+        }
+    }
+    
+    func handleLoginResponse(success: Bool, error: Error?) {
+        if success {
+            print(TMDBClient.Auth.requestToken)
+            TMDBClient.getSessionID(completion: handleSessionResponse(success:error:))
+        } else {
+            showLoginFailure(message: error?.localizedDescription ?? "")
+        }
+    }
+    
+    func handleSessionResponse(success: Bool, error: Error?) {
+        setLogginIn(false)
+        if success {
+            print(TMDBClient.Auth.sessionId)
+            self.performSegue(withIdentifier: "completeLogin", sender: nil)
+        } else {
+            showLoginFailure(message: error?.localizedDescription ?? "")
+        }
+    }
+    
+    func setLogginIn(_ loggingIn: Bool) {
+        if loggingIn {
+            activityIndicator.startAnimating()
+        } else {
+            activityIndicator.stopAnimating()
+        }
+        self.emailTextField.isEnabled = !loggingIn
+        self.passwordTextField.isEnabled = !loggingIn
+        self.loginButton.isEnabled = !loggingIn
+        self.loginViaWebsiteButton.isEnabled = !loggingIn
+    }
+    
+    func showLoginFailure(message: String) {
+        let alertVC = UIAlertController(title: "Login Failed", message: message, preferredStyle: .alert)
+        alertVC.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        show(alertVC, sender: nil)
+    }
+}
